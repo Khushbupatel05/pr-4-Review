b24bfea56db8b85c7e6ec9026d69f714499bb148
khushbupatel05 <khushbupatel0009@gmail.com>
2025-06-03 12:18:22 +0530

    for change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Reviewbox from './assets/Components/Reviewbox'
 
 const App = () => {
--- src/assets/Components/Reviewbox.jsx
+++ src/assets/Components/Reviewbox.jsx
@@ -11,6 +11,12 @@ const Reviewbox = () => {
     }
     const handleSubmit = (e)=>{
        e.preventDefault();
+
+       let showError = {};
+
+       if(input.name.trim()== ""){
+        showError.name = "Enter Your Name"
+       }
        
     }
   return (
@@ -48,12 +54,9 @@ const Reviewbox = () => {
             </div>
 
             <div className="flex items-start mb-5 mt-5">
-                <div className="flex items-center h-5">
-                <input id="terms" type="checkbox" defaultValue className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 " required />
-                </div>
-                <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
-            </div>
+               
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </div>
             </form>
         </div>
     </>
